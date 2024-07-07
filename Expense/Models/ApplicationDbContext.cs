2f886d55647c56ff7621c5f0f9cc57f90de558bb
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-07-07 15:27:19 +0000

    crud opertaion ASP>NET done

---
+++ Expense/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Expense.Models.Expense> Expenses { get; set; }
+    }
+}
---
+++ Expense/Models/Expense.cs
@@ -0,0 +1,10 @@
+﻿namespace Expense.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
